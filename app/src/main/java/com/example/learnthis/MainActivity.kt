135a4fcd6bc92a85f710d058cd6bcac825958d4f
Astrid Bakken <astridbakken95@gmail.com>
2023-01-31 18:34:24 +0100

    Android Studio: Practice Compose, Task Complete Page

--- app/src/main/java/com/example/learnthis/MainActivity.kt
+++ app/src/main/java/com/example/learnthis/MainActivity.kt
@@ -13,11 +13,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.learnthis.ui.theme.LearnThisTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,14 +34,9 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-//@Composable
-//fun Greeting(name: String) {
-//    Text(text = "Te amo $name!")
-//}
-
 @Composable
 fun BannerImage() {
-    val image = painterResource(R.drawable.bg_compose_background)
+    val image = painterResource(R.drawable.ic_task_completed)
     Box {
         Image(
             painter = image,
@@ -50,44 +47,38 @@ fun BannerImage() {
 }
 
 @Composable
-fun ArticleText(title: String, part_1: String, part_2: String) {
+fun CelebrationText(taskC: String, niceW: String) {
     Column {
         Text(
-            text = title,
-            fontSize = 24.sp,
+            text = taskC,
             modifier = Modifier
                 .fillMaxWidth()
-                .wrapContentWidth(Alignment.Start)
-                .padding(16.dp)
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(top = 24.dp, bottom = 8.dp),
+            fontWeight = FontWeight.Bold
         )
         Text(
-            text = part_1,
+            text = niceW,
             textAlign = TextAlign.Justify,
             modifier = Modifier
                 .fillMaxWidth()
-                .wrapContentWidth(Alignment.Start)
-                .padding(start = 16.dp, end = 16.dp)
-
-        )
-        Text(
-            text = part_2,
-            textAlign = TextAlign.Justify,
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentWidth(Alignment.Start)
-                .padding(16.dp)
+                .wrapContentWidth(Alignment.CenterHorizontally),
+            fontSize = 16.sp
         )
     }
 }
 
 @Composable
-fun ArticlePage(title: String, part_1: String, part_2: String) {
-    Column {
+fun CompletePage(taskC: String, niceW: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         BannerImage()
-        ArticleText(
-            title = title,
-            part_1 = part_1,
-            part_2 = part_2
+        CelebrationText(
+            taskC = taskC,
+            niceW = niceW
         )
     }
 }
@@ -96,11 +87,10 @@ fun ArticlePage(title: String, part_1: String, part_2: String) {
 @Composable
 fun TestPreview() {
     LearnThisTheme {
-        ArticlePage(
-            stringResource(R.string.article_title),
-            stringResource(R.string.part_1),
-            stringResource(R.string.part_2)
-        )
+        CompletePage(
+            stringResource(R.string.task_complete),
+            stringResource(R.string.nice_work)
+            )
     }
 }
 
