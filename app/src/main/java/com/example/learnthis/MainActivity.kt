27c2f5191362a77341531605d10e4006c1779dd4
Astrid Bakken <astridbakken95@gmail.com>
2023-02-02 15:16:37 +0100

    Android Studio: Practice Compose, Compose Greeting Card

--- app/src/main/java/com/example/learnthis/MainActivity.kt
+++ app/src/main/java/com/example/learnthis/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.learnthis
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
@@ -12,15 +14,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Cyan
-import androidx.compose.ui.graphics.Color.Companion.Green
-import androidx.compose.ui.graphics.Color.Companion.LightGray
-import androidx.compose.ui.graphics.Color.Companion.Yellow
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learnthis.ui.theme.LearnThisTheme
 
 
@@ -38,62 +37,90 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ComposeQuadrant(
+fun IntroCard(
+        name : String,
         title: String,
-        description: String,
-        backgroundColor: Color,
         modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.contacticon)
     Column (
         modifier = modifier
-            .fillMaxSize()
-            .background(backgroundColor)
-            .padding(16.dp),
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Bottom
             ) {
-        Text(
-            text = title,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 16.dp) //normal Modifier, big M
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.padding(16.dp)
         )
         Text(
-            text = description,
-            textAlign = TextAlign.Justify
+            text = name,
+            fontWeight = FontWeight.Bold,
+            fontSize = 36.sp,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = title,
+            fontSize = 20.sp
+        )
+    }
+}
+@Composable
+fun ContactLine(text: String, @DrawableRes icon: Int) {
+    Row (
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(icon),
+            contentDescription = null,
+            modifier = Modifier.weight(0.2f)
+        )
+        Text (
+            text = text,
+            fontSize = 20.sp,
+            textAlign = TextAlign.End,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.weight(1f).padding(10.dp)
         )
     }
 }
 
 @Composable
+fun ContactCard (
+    number: String,
+    socialMedia: String,
+    email: String,
+    modifier: Modifier = Modifier) {
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+            .padding(30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        ContactLine(number, R.drawable.phonelogo)
+        ContactLine(email, R.drawable.emaillogo)
+        ContactLine(socialMedia, R.drawable.atsignlogopng)
+    }
+
+
+
+}
+
+@Composable
 fun CompletePage() {
-    Column (Modifier.fillMaxWidth()) {
-        Row (Modifier.weight(1f)) {
-            ComposeQuadrant(
-                title = stringResource(R.string.textC),
-                description = stringResource(R.string.textCText),
-                backgroundColor = Green,
+    Column (Modifier.fillMaxWidth().background(color = Color.LightGray)) {
+        IntroCard(
+                name = "Astrid Bakken",
+                title = "Developer",
                 modifier = Modifier.weight(1f)
             )
-            ComposeQuadrant(
-                title = stringResource(R.string.imageC),
-                description = stringResource(R.string.imageCText),
-                backgroundColor = Yellow,
-                modifier = Modifier.weight(1f)
+        ContactCard(
+            number = "+47 41662634",
+            socialMedia = "@ahbakken",
+            email = "ahobakken@gmail.com",
+            modifier = Modifier.weight(1f)
             )
-        }
-        Row (Modifier.weight(1f)) {
-            ComposeQuadrant(
-                title = stringResource(R.string.rowC),
-                description = stringResource(R.string.rowCText),
-                backgroundColor = Cyan,
-                modifier = Modifier.weight(1f)
-            )
-            ComposeQuadrant(
-                title = stringResource(R.string.comulmC),
-                description = stringResource(R.string.comlumnCText),
-                backgroundColor = LightGray,
-                modifier = Modifier.weight(1f)
-            )
-        }
     }
 }
 
