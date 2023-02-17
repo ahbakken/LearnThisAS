bd102be2ab71930a29bd47e375cf2d985f9da714
Astrid Bakken <astridbakken95@gmail.com>
2023-02-17 20:36:05 +0100

    Android Studio Training: LemonadeApp, button training.

--- app/src/main/java/com/example/learnthis/MainActivity.kt
+++ app/src/main/java/com/example/learnthis/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,47 +28,57 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LearnThisTheme {
-                DiceRollerApp()
+                LemonadeApp()
             }
         }
     }
 }
-@Preview
+@Preview(showBackground = true)
 @Composable
-fun DiceRollerApp() {
-    DiceWithButtonAndImage(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
-    )
-}
+fun LemonadeApp() {
 
-@Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result by remember { mutableStateOf(1) }
-    val imageResource = when(result) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
-    }
-
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
     ) {
-        Image(
-            painter = painterResource(id = imageResource),
-            contentDescription = result.toString())
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = {
-            result = (1..6).random()
-        }) {
-            Text(stringResource(R.string.roll))
+        var result by remember { mutableStateOf(1) }
+        val imageResource = when (result) {
+            1 -> R.drawable.lemon_tree
+            in 2..5 -> R.drawable.lemon_squeeze
+            6 -> R.drawable.lemon_drink
+            else -> R.drawable.lemon_restart
+        }
+        val textResource = when (result) {
+            1 -> R.string.LemonTree
+            in 2..5 -> R.string.Lemon
+            6 -> R.string.FullGlass
+            else -> R.string.EmptyGlass
         }
 
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(stringResource(textResource))
+            Spacer(modifier = Modifier.height(16.dp))
+            Image(
+                painter = painterResource(id = imageResource),
+                contentDescription = result.toString(),
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clip(RoundedCornerShape(7.dp))
+                    .clickable(
+                        onClick = {
+                            if (result < 6) {
+                                result++
+                            } else {
+                                result = 0
+                            }
+                        }
+                    )
+                    .border(1.dp, Color.Black, shape = RoundedCornerShape(7.dp))
+            )
+        }
     }
 }
